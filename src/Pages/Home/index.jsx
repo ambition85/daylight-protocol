25a0bda230dd36e63d29b63adbe5e343d4a2fb8c
unknown <mhui0912@gmail.com>
2022-07-23 04:00:53 +0800

    add allowance

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -193,14 +193,16 @@ const Home = () => {
         return;
       }
       if (!!presaleReadContract && !!usdcReadContract) {
-        const [userInfo, whitelisted, claimable, usdcBalance] =
+        const [userInfo, whitelisted, claimable, usdcBalance, usdcAllowance] =
           await Promise.all([
             presaleReadContract.userInfo(wallet),
             presaleReadContract.whitelisted(wallet),
             presaleReadContract.claimableAmount(wallet),
             usdcReadContract.balanceOf(wallet),
+            usdcReadContract.allowance(wallet, PresaleAddress),
           ]);
 
+        setUsdcAllowance(!usdcAllowance.lt(maxTo));
         setTotalDayl(userInfo.totalReward.toString());
         setDepositAmount(userInfo.depositAmount.toString());
         setTotalWithdrawn(userInfo.withdrawnReward.toString());
