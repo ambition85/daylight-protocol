ff3c235bfec7fb23066c640542472787612267ac
unknown <mhui0912@gmail.com>
2022-07-24 22:46:54 +0800

    litepaper link missing

--- src/Blocks/Litepaper/index.jsx
+++ src/Blocks/Litepaper/index.jsx
@@ -8,7 +8,7 @@ const Litepaper = () => {
       <Icon imgsrc={logoMascot} classnamestyle="litepaper-logo" />
       <div className="aic litepaper--info">
         <div className=" litepaper--info-title">Project Daylight</div>
-        <div className="litepaper--button aic">LITEPAPER</div>
+        <a href="https://daylight-protocol.gitbook.io/litepaper/" target="blank" className="litepaper--button aic">LITEPAPER</a>
       </div>
     </div>
   );
